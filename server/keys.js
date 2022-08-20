ace21c78e24c2ceac57ff69ac1f4b9e0c002bd3b
devshakti78 <shakti.dev46@gmail.com>
2022-08-20 21:28:40 +0530

    done

---
+++ server/keys.js
@@ -0,0 +1,7 @@
+const { model } = require("mongoose");
+
+module.exports={
+    MONGOURI:"mongodb+srv://shakti:shakti@cluster0.gepcdbf.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET:"shajsljfadkjf"
+}
+
